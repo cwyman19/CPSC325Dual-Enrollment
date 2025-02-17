9b603ea3cd86fba6c3d67f64b44ccffc1f7987be
jberry03 <berryjil000@gmail.com>
2025-02-17 15:52:11 -0800

    Sentence Transformer

---
+++ excelConverter.py
@@ -0,0 +1,17 @@
+import openpyxl
+
+# Load the workbook
+workbook = openpyxl.load_workbook('2024-25CTEArticulations.xlsx')
+
+# Get the sheet by name or index
+sheet = workbook['Sheet1']  # Replace 'Sheet1' with your sheet name
+# sheet = workbook.active # To get the active sheet
+
+data_list = []
+for row in sheet.iter_rows(values_only=True):
+    data_list.append(list(row))
+print(data_list[2][0])
+# # Now data_list contains all rows as lists
+# print(data_list)
+
+# print(data_list[2])
