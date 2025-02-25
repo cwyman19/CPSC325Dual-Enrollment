66e350b8398459397df0d98f84d20bf7b6cb18a5
jberry03 <berryjil000@gmail.com>
2025-02-25 12:49:04 -0800

    Frontend

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
+import logo from './image.png';
 import './App.css';
+import React, { useState } from 'react';
+
 
 function App() {
+  const [isExpanded, setIsExpanded] = useState(false);
+
+  
+   // Function to toggle the expansion state
+   const toggleExpansion = () => {
+    setIsExpanded(!isExpanded);
+  };
+  
   return (
     <div className="App">
-      <header className="App-header">
-        {/* <img src={logo} className="App-logo" alt="logo" /> */}
-        {/* <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p> */}
-        <a
+    <header className="App-header">
+      <img src={logo} className="logo" alt="logo" />
+      {/* className="App-logo" */}
+      {/* <p>
+        Edit <code>src/App.js</code> and save to reload.
+      </p> */}
+       <a
           className="App-link"
           // href="https://reactjs.org"
           // target="_blank"
           // rel="noopener noreferrer"
         >
-        Dual Enrollment
+        
         </a>
-      </header>
+     
+    </header>
+    <div
+        className={`expandable-div ${isExpanded ? 'expanded' : ''}`}
+      >
+    <form id="courseForm">
+        <label for="fname">High School Course Name:</label><br></br>
+        <input type="text" id="fname" name="fname"></input><br></br>
+        <label for="lname">High School Course Description:</label><br></br>
+        <textarea rows="5" cols="33">
+              Enter a High School Course Description
+        </textarea><br></br>
+        <button onClick={toggleExpansion}>  Submit </button> 
+    </form>
     </div>
+  </div>
   );
+  
+  
+  
 }
+
 
 export default App;
