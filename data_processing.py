39cfbf6cfdab17b43a058f71ad39b98aee022e2f
cwyman19 <wymancharlie19@gmail.com>
2025-03-15 21:05:54 -0700

    added more data processing capabilities

--- data_processing.py
+++ data_processing.py
@@ -1,9 +1,12 @@
 '''
 Data Processing for dual enrollment data
-uses MyPyTable objects
+uses pandas dataframe objects
 '''
+
 import pandas as pd
 import numpy as np
+import os
+import time
 
 #my_raw_dataset = MyPyTable().load_from_file("input_data/Dual-Enrollment-Data.csv")
 
@@ -75,22 +78,109 @@ print(df)
 df.to_csv("output_data.csv")
 '''
 
-input_filename = input("What is the filename of the input data? (must be a csv format) ")
+print("Data Processing: ")
+print(" 1. Add new courses to the dataset via a csv upload.")
+print(" 2. Drop duplicate rows from dataset.")
+print(" 3. Make a backup of the current course dataset.")
+print(" 4. Restore old backup.")
 
-input_df = pd.read_csv(f"input_data/{input_filename}")
-old_df = pd.read_csv("output_data/output_course_data.csv")
 
-for i in range(len(input_df)):
-    new_row = input_df.iloc[i]
-    new_row = new_row.to_numpy()
-    #print("new row: ", new_row)
-    old_df.loc[-1] = new_row
-    old_df.index = old_df.index + 1
-    
+input_data_function = input("What data processing function would you like to run? (Enter a number, or Q to quit): ")
 
-print("new dataframe: ")
-print(old_df)
-old_df.to_csv("output_data/output_course_data.csv", index=False)
+while (input_data_function not in ['Q', '1', '2', '3', '4']):
+    input_data_function = input("Invalid, please enter a valid function number (or Q to quit): ")
+
+
+while (input_data_function == '1'):
+    input_filename = input("What is the filename of the input data? (must be a csv format) ")
+
+    input_df = pd.read_csv(f"input_data/{input_filename}")
+    old_df = pd.read_csv("output_data/output_course_data.csv")
+
+    for i in range(len(input_df)):
+        new_row = input_df.iloc[i]
+        new_row = new_row.to_numpy()
+        #print("new row: ", new_row)
+        old_df.loc[-1] = new_row
+        old_df.index = old_df.index + 1
+        
+
+    #print("new dataframe: ")
+    #print(old_df)
+    old_df.to_csv("output_data/output_course_data.csv", index=False)
+
+    print("Your data has been added to the dataset")
+    print()
+    yes_no_input = input("Would you like to make further changes to the dataset? (Y/N)" )
+    if yes_no_input == 'Y' or yes_no_input == 'y':
+        print(" 1. Add new courses to the dataset via a csv upload.")
+        print(" 2. Drop duplicate rows.")
+        print(" 3. Make a backup of the current course dataset.")
+        input_data_function = input("What data processing function would you like to run? (Enter a number, or Q to quit): ")
+    else:
+        input_data_function = 'Q'
+
+
+if (input_data_function == '2'):
+    old_df = pd.read_csv("output_data/output_course_data.csv")
+    old_df = old_df.drop_duplicates()
+    old_df.to_csv("output_data/output_course_data.csv", index=False)
+    print("Duplcates have been removed from the dataset")
+    print()
+
+if (input_data_function == '3'):
+    folder_path = "output_data/"
+    num_files = len([f for f in os.listdir(folder_path) if os.path.isfile(os.path.join(folder_path, f))])
+    num_time = time.time_ns()
+    if (num_files >= 20): #olds backups will be rewritten after 19 backups have been created
+        filenames = []
+        for filename in os.listdir(folder_path):
+            file_path = os.path.join(folder_path, filename)
+            if (filename.startswith("courses_backup_num")):
+                filenames.append(filename)
+        old_file = min(filenames)
+        print(f"The backups folder is full, this process will overwite the oldest backup ({old_file})")
+        confirmation_input = input("Continue anyway? (Y/N) ")
+        if (confirmation_input == 'y') or (confirmation_input == 'Y'):
+            old_df = pd.read_csv("output_data/output_course_data.csv")
+            old_df.to_csv(f"output_data/{old_file}", index=False)
+            new_filename = f"courses_backup_num{num_time}.csv"
+            os.rename(f"output_data/{old_file}", f"output_data/{new_filename}")
+            print("new filename: ", new_filename)
+            print("Backup Finished")
+        else:
+            print("Process cancelled")
+    else:
+        old_df = pd.read_csv("output_data/output_course_data.csv")
+        new_filename = f"courses_backup_num{num_time}.csv"
+        old_df.to_csv(f"output_data/{new_filename}", index=False)
+        print("new filename: ", new_filename)
+        print("Backup Finished")
+
+
+if (input_data_function == '4'):
+    print()
+    print("This process is irreversible.")
+    input_confirm = input("Are you sure you want to overwrite the course datafile with the latest backup? (Y/N) ")
+    if (input_confirm == 'Y') or (input_confirm == 'y'):
+        folder_path = "output_data/"
+        filenames = []
+        for filename in os.listdir(folder_path):
+            file_path = os.path.join(folder_path, filename)
+            if (filename.startswith("courses_backup_num")):
+                filenames.append(filename)
+        backup_file = max(filenames)
+        old_df = pd.read_csv(f"output_data/{backup_file}")
+        old_df.to_csv(f"output_data/output_course_data.csv", index=False)
+        print("Backup Restored.")
+    else:
+        print("Backup restoration cancelled.")
+
+        
+        
+
+
+
 #print(input_df)
 #print(new_df)
 new_row_indeces = {"College": 0, "College District": 1, "Articulation": 2, "College Program": 3, "Career Cluster": 4, "College Course": 5, "College Course Name": 6, 
@@ -105,7 +195,3 @@ new_row_indeces = {"College": 0, "College District": 1, "Articulation": 2, "Coll
 #print(" --------------------------- ")
 #print("new dataframe + new row: ")
 #print(new_df)
-
-#new_df.to_csv("output_data2.csv", index=False)
-# TODO: concatenate college course name and college course number together if both fields are filled (for algorithm).
-
